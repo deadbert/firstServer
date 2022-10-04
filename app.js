81a453171bab2097572e382a0257f154341b08e8
Nick_Spencer <strangebrew202@gmail.com>
2022-10-04 13:50:08 -0600

    set up server using ejs templating engine

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const path = require('path');
 
 const app = express();
 
-app.engine('handlebars', expressHbs.engine({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -20,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {layout: false, pageTitle: 'Page not Found'});
+    res.status(404).render('404', {pageTitle: 'Page not Found'});
 });
 
 app.listen(3000)
